bf15bd88da2a795b75ba10b0d57a0dc99b4466fe
fly <fly>
2017-08-20 17:03:35 +0800

    p8提交

---
+++ p8订阅阅读器测试/jasmine/src/feedreader.js
@@ -0,0 +1,17 @@
+var menu_hidden = $('body').hasClass('menu-hidden'),
+    menuIcon = $('.menu-icon-link').
+    changesFeeds = [
+    {
+        name: 'baiDu',
+        url: 'https://www.baidu.com'
+    }
+];
+
+menuIcon.on('click', function() {
+    if($('body').hasClass('menu-hidden')){
+        var isHidden = true;
+    }else{
+        var isShow = true;
+    }
+  
+});
