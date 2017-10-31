cf8d04342f920b7ed8736eeec69f637669c6c022
fly <fly>
2017-10-31 23:36:17 +0800

    p7修改

--- p7街区地图/js/ko.js
+++ p7街区地图/js/ko.js
@@ -1,39 +1,26 @@
-//store the initial location
-var initLocations = [{
-    title: '天安门',
-    position: [116.39756, 39.908808]
-}, {
-    title: '前门',
-    position: [116.397994, 39.900085]
-}, {
-    title: '天坛公园',
-    position: [116.410886, 39.881998]
-}, {
-    title: '颐和园',
-    position: [116.272852, 39.992273]
-}, {
-    title: '鸟巢',
-    position: [116.396203, 39.993575]
-}];
-
-var locations = initLocations;
-
 var ViewModel = function() {
-    this.content = ko.observable("");
-    this.locations = ko.observableArray(initLocations);
-    this.clickPoint = function(point){
-      console.log('clickPoint', point);
-      showPointInfo(point);
+    var isMin = $(window).width() > 750 ? false : true;
+	this.isMin = ko.observable(isMin);
+    this.isShow = ko.observable(false);
+    this.content = ko.observable("杭州");
+    this.locations = ko.observableArray();
+    this.clickPoint = function(point) {
+        showPointInfo(point);
     };
-    this.result = ko.computed(function(){
-      var self = this;
-      var filterArr = this.locations().filter(function(e){
-        return e.title.indexOf(self.content())>=0
-      });
-      locations = filterArr;
-      renderMap();
-      return filterArr;
-    }, this)
-};
 
+    this.search = function() {
+        var content = this.content();
+        search(content, data => {
+            this.locations(data);
+            mark(this.locations());
+        });
+    };
+
+    this.show = function() {
+        var isMin = this.isMin();
+        this.isMin(!isMin);
+        this.isShow(isMin);
+    };
+
+};
 ko.applyBindings(new ViewModel()); // This makes Knockout get to work
